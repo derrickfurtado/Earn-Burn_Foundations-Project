a2d99d60e4c5ee810b910c3cf0f5388e81089d12
Derrick Furtado <derrick.furtado10@gmail.com>
2024-03-06 15:08:25 -0700

    adding elements to main.html

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ const loginAction = (event) => {
     axios.post('http://localhost:4040/api/login',loginProcess)
     .then(res => {
         window.location.href='./main.html'
-    })
+    }).catch(err => alert("Username or password is incorrect"))
 }
 
 
